75f0c0a7dba5da4aa40d2599c5237812b6978dcd
MorskoySom <Somovvitvit@gmail.com>
2023-11-10 14:47:22 +0200

    55511

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchContacts } from "./Redux/operations";
-import { getContacts, getIsLoading, getError } from "./Redux/selectors";
+import { getIsLoading, getError } from "./Redux/selectors";
 
 import { ContactList } from "ContactsList/ContactsList";
 import { ContactForm } from "ContactForm/ContactForm";
@@ -13,18 +13,13 @@ export const App = () => {
   const isLoading = useSelector(getIsLoading);
   const error = useSelector(getError);
 
-  const contacts = useSelector(getContacts);
-  // const { items, isLoading, error } = contacts;
-  console.log(contacts.map(contact => contact.name));
-  console.log(contacts.map(contact => contact.phone));
-  // Вызываем операцию
-
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
 
   return (
     <Container>
+      {isLoading && !error && <b>Request in progress...</b>}
       <h1>Phonebook</h1>
       <ContactForm />
       <h2>Contacts</h2>
@@ -32,18 +27,6 @@ export const App = () => {
       <ContactList />
     </Container>
   )
-
-  // // Рендерим разметку в зависимости от значений в состоянии
-  // return (
-  //   <div>
-  //     {/* {isLoading && <p>Loading tasks...</p>} */}
-  //     {/* {error && <p>{error}</p>} */}
-  //     <p>{contacts.length > 0 && JSON.stringify(contacts, null, 2)}</p>
-  //   </div>
-  // );
-
-
-
 };
 
 
